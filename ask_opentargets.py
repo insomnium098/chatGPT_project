28c3d3c9530d9e978e412247d082bc2dc2d93c61
insomnium098 <insomnium098@gmail.com>
2023-07-14 14:30:02 -0600

    Uncomment code

--- ask_opentargets.py
+++ ask_opentargets.py
@@ -37,34 +37,34 @@ response = openai.ChatCompletion.create(#openai.Completion.create(
 # Accessing the output of chatgpt
 response_text = response["choices"][0]["message"]["content"]
 
-# query_string = prime_prompt + response_text
+query_string = prime_prompt + response_text
 
-# # filename with current date and time
-# query_file = "query_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
+# filename with current date and time
+query_file = "query_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
 
-# # write query to file with current date and time
-# with open(query_file, "w") as f:
-#     f.write(f"# User input: {user_input}\n")
-#     f.write(query_string)
-#     print(f"\nCustom graphQL query was written to file: {query_file}")
+# write query to file with current date and time
+with open(query_file, "w") as f:
+    f.write(f"# User input: {user_input}\n")
+    f.write(query_string)
+    print(f"\nCustom graphQL query was written to file: {query_file}")
 
-# # Set base URL of GraphQL API endpoint
-# base_url = "https://api.platform.opentargets.org/api/v4/graphql"
+# Set base URL of GraphQL API endpoint
+base_url = "https://api.platform.opentargets.org/api/v4/graphql"
 
-# # Perform POST request and check status code of response
-# # This handles the cases where the Open Targets API is down or our query is invalid
-# try:
-#     response = requests.post(base_url, json={"query": query_string})
-#     response.raise_for_status()
-# except requests.exceptions.HTTPError as err:
-#     print(err)
+# Perform POST request and check status code of response
+# This handles the cases where the Open Targets API is down or our query is invalid
+try:
+    response = requests.post(base_url, json={"query": query_string})
+    response.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
 
-# # Transform API response from JSON into Python dictionary and print in console
-# api_response = json.loads(response.text)
-# hits_list = api_response["data"]["search"]["hits"][0]
+# Transform API response from JSON into Python dictionary and print in console
+api_response = json.loads(response.text)
+hits_list = api_response["data"]["search"]["hits"][0]
 
-# print("\n\nQuerying Open Targets genetics database...\n\n")
+print("\n\nQuerying Open Targets genetics database...\n\n")
 
-# disease_list = extract_values(hits_list, "disease")
-# for i, j in enumerate(disease_list):
-#     print(f"{i+1}. {j['name']}")
+disease_list = extract_values(hits_list, "disease")
+for i, j in enumerate(disease_list):
+    print(f"{i+1}. {j['name']}")
